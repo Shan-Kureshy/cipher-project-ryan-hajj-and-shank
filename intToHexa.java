0183b0694f8a4eb7f170dcd53475ba763c68f6b5
RyanHajj <hadihunterz@gmail.com>
2016-05-11 13:12:15 -0700

    update
    5/11 ryan

---
+++ intToHexa.java
@@ -0,0 +1,20 @@
+
+public class intToHexa {
+	static int num = 15;
+	
+	public static String intToHex(int num)
+	{
+		
+		
+		
+		return (Integer.toHexString(num));
+		
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(intToHex(num));
+	}
+}
+
